d2d5f26adc62dbb77fc061c7f97ceb1d7719643c
i-roger <mroger.dev@gmail.com>
2024-06-14 18:35:40 -0300

    Mais updates

--- app/src/app/components/navbar.tsx
+++ app/src/app/components/navbar.tsx
@@ -1,7 +1,19 @@
 export default function Navbar() {
     return(
-    <div className="bg-yellow-200">
-            <p className="text-red-600">Navbar</p>
+    <div className="flex justify-between items-center px-4 py-4 text-[27px] max-sm:hidden">
+        <div className="">
+            <a>MC</a>
+        </div>
+        <div>
+            <ul className="flex flex-row items-center gap-4">
+                <li className="bg-red-600 p-2 text-yellow-300 font-extrabold"><a>Compre Agora!</a></li>
+                <li><a>Reserva</a></li>
+                <li><a>Galeria</a></li>
+                <li><a>Entre em contato</a></li>
+                <li><a>LOJA?</a></li>
+            </ul>
+        </div>
     </div>
     )
 }
+//Localização , reserva, galeria, loja, entre em contato, compre agora!
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -6,12 +6,29 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <main className="flex justify-center">
+    <main className="">
       <Navbar />
     </main>
     <section>
       <Card />
       <Anuncio />
+    </section>
+    <section className="flex p-5 justify-center bg-slate-200">
+    <div className="w-[330px] h-[440px] flex flex-col bg-white rounded ring-[1px] drop-shadow-xl ring-gray-300">
+      
+      <div className="h-[400px] p-4 text-black">
+          <h1 className="text-[32px]">Hello World</h1>
+          <p>asdasdas</p>
+        </div>
+      <div className="w-[330px] h-[70px] p-4 flex justify-between text-black">
+        <div id="preço" className="">
+          <a>R$79,99</a>
+        </div>
+        <div className="flex items-center justify-center px-4 py-5 bg-[#0070F0] text-white rounded-full">
+          <p>Comprar</p>
+        </div>
+      </div>
+    </div>
     </section>
     </>
   );
